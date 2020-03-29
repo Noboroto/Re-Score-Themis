b3fd8c6f36db8c03d22a686d63a54c7610bee35b
David0403 (Gaming- PC) <48942146+Noboroto@users.noreply.github.com>
2020-03-29 11:46:39 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace ReScoreThemis
             }
             else
             {
+                if (Directory.Exists(MainPath + "\\Logs")) Directory.Delete(MainPath + "\\Logs", true);
                 ReScore(MainPath);
             }
         }
